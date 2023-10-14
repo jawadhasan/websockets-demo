8246abfc77d7241029757cc9ef2ac903a24a88a9
Jawad Shani <jawad.shani@hitachi-eu.com>
2023-10-14 15:01:23 +0200

    server init

---
+++ server/SocketServer/SocketServer/Worker.cs
@@ -0,0 +1,17 @@
+namespace SocketServer
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+        }
+    }
+}
